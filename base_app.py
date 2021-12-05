4e0ee110dcff0329038ac9725ae7b33dc7f13d19
marth418 <88935604+marth418@users.noreply.github.com>
2021-12-05 19:12:45 +0200

    Pickle files

--- base_app.py
+++ base_app.py
@@ -29,8 +29,8 @@ import joblib,os
 import pandas as pd
 
 # Vectorizer
-vectorizer = open("resources/vector.pkl","rb")
-tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
+vectorizer = open("resources/CountVect","rb")
+tweet_cv = joblib.load(vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
@@ -70,7 +70,7 @@ def main():
 			vect_text = tweet_cv.transform([tweet_text]).toarray()
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/logisticRe.pkl"),"rb"))
+			predictor = joblib.load(open(os.path.join("resources/LogisticClassifier.pkl"),"rb"))
 			prediction = predictor.predict(vect_text)
 
 			# When model has successfully run, will print prediction
